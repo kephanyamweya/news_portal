021661ebb5705e9877ca576a0d7d82fed285ba9a
kephanyamweya <kephanyamweya95@gmail.com>
2021-08-26 15:44:39 +0300

    add User.java to the project

---
+++ src/main/java/models/User.java
@@ -0,0 +1,56 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String name;
+    private String role;
+    private int departmentId;
+
+    public User(String name, String role, int departmentId) {
+        this.name = name;
+        this.role = role;
+        this.departmentId = departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                departmentId == user.departmentId &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, role, departmentId);
+    }
+}
